d42ebdf3af9eeefd063977dd250818782f391849
Morzat95 <fe_de_qui9@hotmail.com>
2024-12-26 16:29:09 -0300

    Update query selector

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 setInterval(() => {
 	const buttons = document.querySelectorAll(
-		"button.ScCoreButton-sc-ocjdkq-0.bCfhNy, button.ScCoreButton-sc-ocjdkq-0.bNaSBh"
+		"button.ScCoreButton-sc-ocjdkq-0.hpBkMI, button.ScCoreButton-sc-ocjdkq-0.gyiGhH"
 	);
 	if (buttons.length > 0) {
 		buttons[0].click();
